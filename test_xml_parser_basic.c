d3feb72572de66e3d34be6b7448737044f5690d4
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2017-07-29 10:18:01 +0200

    Renamed nodes to improve error printouts
    - Fixed NextNode test
    - Renamed text_buffer to xml_string

--- test_xml_parser_basic.c
+++ test_xml_parser_basic.c
@@ -4,15 +4,26 @@
 
 #define RESPONSE_MAX_LENGTH 200
 extern char *msg;
-char text_buffer[RESPONSE_MAX_LENGTH] = "<MainNode><BareNode>test_value</BareNode><ParentNode><SimpleNode/><ChildNode>ChildValue</ChildNode></ParentNode></MainNode>";
+char xml_string[] = 
+  "<NODE_0>"
+    "<NODE_0_0>"
+      "NODE_0_0_VALUE"
+    "</NODE_0_0>"
+    "<NODE_0_1>"
+      "<NODE_0_1_0/>"
+      "<NODE_0_1_1>"
+        "NODE_0_1_1_VALUE"
+      "</NODE_0_1_1>"
+    "</NODE_0_1>"
+  "</NODE_0>";
 XML_Node xmlNode, xmlNodeFound;
 
 /**
  * Find the index after a match of a strstr match
  */
 static inline int indexAfterMatch(const char *haystack, const char *needle) {
-  int matchPos = haystack - strstr(haystack, needle);
-  if (matchPos >= 0 and matchPos < (signed)(strlen(haystack) - strlen(needle))) {
+  int matchPos = strstr(haystack, needle) - haystack;
+  if (matchPos >= 0 and matchPos < (signed)(strlen(needle) - strlen(haystack))) {
     matchPos += sizeof(needle) + 1;
   }
   return matchPos;
@@ -21,7 +32,7 @@ static inline int indexAfterMatch(const char *haystack, const char *needle) {
 TEST_GROUP(XML_PARSER_BASIC);
 
 TEST_SETUP(XML_PARSER_BASIC) {
-  xmlNode = XML_Node(text_buffer);
+  xmlNode = XML_Node(xml_string);
   xmlNodeFound = XML_Node();
 }
 
@@ -29,9 +40,9 @@ TEST_TEAR_DOWN(XML_PARSER_BASIC) {
 }
 
 TEST(XML_PARSER_BASIC, ConstructorString) {
-  TEST_ASSERT_EQUAL_STRING(text_buffer, xmlNode.getString());
+  TEST_ASSERT_EQUAL_STRING(xml_string, xmlNode.getString());
   TEST_ASSERT_EQUAL(0, xmlNode.getStart());
-  TEST_ASSERT_EQUAL(strlen(text_buffer), xmlNode.getEnd());
+  TEST_ASSERT_EQUAL(strlen(xml_string), xmlNode.getEnd());
 }
 
 TEST(XML_PARSER_BASIC, CanNotFindGibberish) {
@@ -40,18 +51,19 @@ TEST(XML_PARSER_BASIC, CanNotFindGibberish) {
 
 TEST(XML_PARSER_BASIC, CanNotFindPartialName) {
   const char *message = "Node should not be found (0)";
-  TEST_ASSERT_NOT_EQUAL_MESSAGE(0, xmlNode.findChild(xmlNodeFound, "Bare"), message);
-  TEST_ASSERT_NOT_EQUAL_MESSAGE(0, xmlNode.findChild(xmlNodeFound, "Node"), message);
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(0, xmlNode.findChild(xmlNodeFound, "NODE_"), message);
+  TEST_ASSERT_NOT_EQUAL_MESSAGE(0, xmlNode.findChild(xmlNodeFound, "node_0"), message);
 }
 
 TEST(XML_PARSER_BASIC, FirstChild) {
   TEST_ASSERT_EQUAL(0, xmlNode.findFirstChild(xmlNodeFound));
-  TEST_ASSERT_EQUAL(indexAfterMatch(text_buffer, "<MainNode>"), xmlNodeFound.getStart());
+  TEST_ASSERT_EQUAL(indexAfterMatch(xml_string, "<NODE_0>"), xmlNodeFound.getStart());
 }
 
 TEST(XML_PARSER_BASIC, NextNode) {
+  xmlNode.findFirstChild(xmlNode);
   TEST_ASSERT_EQUAL(0, xmlNode.findNextNode(xmlNodeFound));
-  TEST_ASSERT_EQUAL(indexAfterMatch(text_buffer, "<MainNode>"), xmlNodeFound.getStart());
+  TEST_ASSERT_EQUAL(indexAfterMatch(xml_string, "</NODE_0_0>"), xmlNodeFound.getStart());
 }
 
 TEST_GROUP_RUNNER(XML_PARSER_BASIC) {
